db03cb39c45d0d399e01517456c17b0b343f8bb6
LuckysBones <LuckysBones@gmail.com>
2021-04-25 15:24:19 -0600

    Pre/post condi

--- EscapeDeep.cpp
+++ EscapeDeep.cpp
@@ -13,11 +13,14 @@ namespace CSC234 {
     EscapeDeep* EscapeDeep::makeGame() {
         return new EscapeDeep;
     }
-
+    //Pre-Condition - user or program runs the start function
+    //Post-Condition - the function will start the private run function
+        
     void EscapeDeep::start() {
         run();
     }
-
+    //Pre-Condition - the main running function for the game "Escape the Deep"
+    //Post-Condition - the run function will start the game and run the associated functions
     void EscapeDeep::run() {
         cout << "Escape the Deep - Run";
         intro();

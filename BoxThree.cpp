fa25f2a5de437107227fb73e26aa2288b584da20
LuckysBones <LuckysBones@gmail.com>
2021-04-25 22:41:07 -0600

    added box 3 sides

--- BoxThree.cpp
+++ BoxThree.cpp
@@ -1,5 +1,7 @@
 #include "BoxThree.h"
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,13 +13,31 @@ void BoxThree::boxThreeSides() {
 }
 
 void BoxThree::B3_sideOne() {
-	cout << "\n\nYou see a '    ' what would you like to do? ";
+	cout << "\n\nYou look at one of the sides and see three objects\n\nwhat would you like to look at? ";
+	vector<string>B3_sideOneOpt = {"Knob", "Lever", "Pannel" };
+	cout << endl;
+	for (size_t i = 0; i < 3; i++) {
+		cout << "\n*" << B3_sideOneOpt[i];
+		//cin >> sideChoice;
+	}
 }
 
 void BoxThree::B3_sideTwo() {
-	cout << "\n\nYou see a '    ' what would you like to do? ";
+	cout << "\n\nYou look at one of the sides and see three objects\n\nwhat would you like to look at? ";
+	vector<string>B3_sideTwoOpt = { "Lever", "Speaker", "Microphone" };
+	cout << endl;
+	for (size_t i = 0; i < 3; i++) {
+		cout << "\n*" << B3_sideTwoOpt[i];
+		//cin >> sideChoice;
+	}
 }
 
 void BoxThree::B3_sideThree() {
-	cout << "\n\nYou see a '    ' what would you like to do? ";
+	cout << "\n\nYou look at one of the sides and see three objects\n\nwhat would you like to look at? ";
+	vector<string>B3_sideThreeOpt = { "Un-lit Light", "Window", "Wires" };
+	cout << endl;
+	for (size_t i = 0; i < 3; i++) {
+		cout << "\n*" << B3_sideThreeOpt[i];
+		//cin >> sideChoice;
+	}
 }
--- BoxThree.h
+++ BoxThree.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
 class BoxThree
 {
 public:
@@ -7,5 +11,10 @@ private:
 	void B3_sideOne();
 	void B3_sideTwo();
 	void B3_sideThree();
+
+	int sideChoice = NULL;
+	//vector <string> B3_sideOneOpt = { "Knob", "Lever", "Pannel" };
+	//vector<string>B3_sideTwoOpt = { "Lever", "Speaker", "Microphone" };
+	//vector<string>B3_sideThreeOpt = { "Un-lit Light", "Window", "Wires" };
 };
 
